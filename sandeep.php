c8525f6c147a23cc8dd1f60b787dd93e15854b2b
rsingh0186 <rsingh@myalpstory.com>
2022-06-12 16:56:58 +0530

    Adding Lorem ipsum

--- sandeep.php
+++ sandeep.php
@@ -2,3 +2,7 @@ Hello Sandeep this is testing of git
 
 
 This is code from my local kindly check commit update working or not.
+
+
+
+Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved. It originally comes from a Latin text, but to today's reader, it's seen as gibberish
